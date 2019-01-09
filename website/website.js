cb1447556ff286c4f7e0e8781a13fce72a7f4a4c
voidev <voydevichb@gmail.com>
2019-01-09 15:29:42 +0200

    gh-pages update

--- website/website.js
+++ website/website.js
@@ -2,3 +2,16 @@
 
 const program = require('commander');
 const path = require('path');
+
+
+program
+    .usage('[command] [options] \n         Command without flags will be started in interactive mode.');
+
+program
+    .command('init')
+    .description('Init TARS-project in current directory')
+    .option('--exclude-html', 'Prevent templater-files uploading')
+    .option('--exclude-css', 'Prevent preprocessor-files uploading')
+    .option('--silent', 'TARS will not ask any question about configuration')
+    .option('-s, --source <source>', 'Change source of TARS')
+    .action(options => require('../lib/command-actions/init')(options));
